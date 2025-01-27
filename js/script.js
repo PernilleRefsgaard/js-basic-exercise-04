0625c5f78d71787b523775849a3839141eb58769
PernilleRefsgaard <Refsgaardpernille@gmail.com>
2025-01-27 09:33:00 +0100

    løsning

---
+++ js/script.js
@@ -0,0 +1,7 @@
+`use strict`;
+
+const welcomeText = `I'm learning to code with JavaScript`;
+
+const resignmentText =`I want to become a good JavaScript programmer`;
+
+console.log(`${welcomeText}`);
